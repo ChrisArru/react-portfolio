6bd61fcd346ed2dacaede7d55ed915da37af3323
Christian Arruzza <>
2023-03-08 16:53:21 +0100

    test build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Hello world this is my website</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. E io che ne so.
         </p>
         <a
           className="App-link"
