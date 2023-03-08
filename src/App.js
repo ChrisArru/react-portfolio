46a3a60e699531c52873669e7e4bc0ad7443adbd
Christian Arruzza <>
2023-03-08 17:29:53 +0100

    header and hero section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./App.css";
-import logo from "./logo.svg";
+import Header from "./views/Header";
+import Main from "./views/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Hello world this is my website</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. E io che ne so.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/views/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+import style from "./Main.module.css";
+
+const Main = () => {
+  return (
+    <section className={style.bgimage} /* class="bgimage" */ id="home">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
+            <h2 className={style["hero_title"]} /* class="hero_title" */>
+              Hi, it's me Brad
+            </h2>
+            <p className={style["hero_desc"]} /* class="hero_desc" */>
+              I am a professional freelancer in New York City
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Main;
