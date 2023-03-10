6195fd8850b23e5909fb23c4c6093a0bc062f949
Christian Arruzza <christian.arruzza@gmail.com>
2023-03-10 11:19:40 +0100

    feat: add Portfolio section

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 import About from "./views/About";
+import Contact from "./views/Contact";
+import Footer from "./views/Footer";
 import Header from "./views/Header";
 import Main from "./views/Main";
+import Portfolio from "./views/Portfolio";
 import Services from "./views/Services";
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
       <Main />
       <About />
       <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/views/Portfolio.js
@@ -0,0 +1,160 @@
+import React from "react";
+import portfolioImage1 from "../images/portfolioImage1.jpg";
+import portfolioImage2 from "../images/portfolioImage2.jpg";
+import portfolioImage3 from "../images/portfolioImage3.jpg";
+import portfolioImage4 from "../images/portfolioImage4.jpg";
+import portfolioImage5 from "../images/portfolioImage4.jpg";
+import portfolioImage6 from "../images/portfolioImage4.jpg";
+
+const Portfolio = () => {
+  return (
+    <section id="portfolio">
+      <div class="container mt-3">
+        <h1 class="text-center">Portfolio</h1>
+        <div class="row">
+          <div class="col-lg-4 mt-4">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src={portfolioImage1}
+                alt="Card1"
+                style={{ width: "100%" }}
+              />
+              <div class="card-body">
+                <h4 class="card-title">YouTube Clone</h4>
+                <p class="card-text">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </p>
+                <div class="text-center">
+                  {/* <a href="#" class="btn btn-success">
+                    Link
+                  </a> */}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4">
+            <div class="card portfolioContent">
+              <img
+                class="card-img-top"
+                src={portfolioImage2}
+                alt="Card2"
+                style={{ width: "100%" }}
+              />
+              <div class="card-body">
+                <h4 class="card-title">Quiz App</h4>
+                <p class="card-text">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </p>
+                <div class="text-center">
+                  {/* <a href="#" class="btn btn-success">
+                    Link
+                  </a> */}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4">
+            <div class="card portfolioContent">
+              <img
+                class="card-img-top"
+                src={portfolioImage3}
+                alt="Card3"
+                style={{ width: "100%" }}
+              />
+              <div class="card-body">
+                <h4 class="card-title">Product Landing Page</h4>
+                <p class="card-text">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </p>
+                <div class="text-center">
+                  {/* <a href="#" class="btn btn-success">
+                    Link
+                  </a> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div class="row">
+          <div class="col-lg-4 mt-4">
+            <div class="card portfolioContent">
+              <img
+                class="card-img-top"
+                src={portfolioImage4}
+                alt="Card4"
+                style={{ width: "100%" }}
+              />
+              <div class="card-body">
+                <h4 class="card-title">Messaging Service</h4>
+                <p class="card-text">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </p>
+                <div class="text-center">
+                  {/* <a href="#" class="btn btn-success">
+                    Link
+                  </a> */}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4">
+            <div class="card portfolioContent">
+              <img
+                class="card-img-top"
+                src={portfolioImage5}
+                alt="Card5"
+                style={{ width: "100%" }}
+              />
+              <div class="card-body">
+                <h4 class="card-title">Twitter Clone</h4>
+                <p class="card-text">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </p>
+                <div class="text-center">
+                  {/* <a href="#" class="btn btn-success">
+                    Link
+                  </a> */}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4">
+            <div class="card portfolioContent">
+              <img
+                class="card-img-top"
+                src={portfolioImage6}
+                alt="Card6"
+                style={{ width: "100%" }}
+              />
+              <div class="card-body">
+                <h4 class="card-title">Blog App</h4>
+                <p class="card-text">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </p>
+                <div class="text-center">
+                  {/* <a href="#" class="btn btn-success">
+                    Link
+                  </a> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
