81cb31e9307f0eedd5c03ae24759f8da39fdffe1
Christian Arruzza <christian.arruzza@gmail.com>
2023-03-10 16:18:03 +0100

    feat: changed Main section with my information

--- src/views/Main.js
+++ src/views/Main.js
@@ -9,10 +9,10 @@ const Main = () => {
           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div className={style["hero-text"]}>
               <h2 className={style["hero_title"]} /* class="hero_title" */>
-                Hi, it's me Brad
+                Hi, I'm Christian.
               </h2>
               <p className={style["hero_desc"]} /* class="hero_desc" */>
-                I am a professional freelancer in New York City
+                Front End Developer based in Turin, Italy.
               </p>
             </div>
           </div>
