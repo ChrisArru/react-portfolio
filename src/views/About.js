11095f4b67daf881cd837f6142c921455d10d32d
Christian Arruzza <christian.arruzza@gmail.com>
2023-03-10 16:17:27 +0100

    feat: change about section with my information

--- src/views/About.js
+++ src/views/About.js
@@ -6,52 +6,41 @@ const About = () => {
   return (
     <section id="about" className={style.about}>
       <div class="container mt-4 pt-4">
-        <h1 class="text-center">About Me</h1>
+        <h1 class="text-center" style={{ marginBottom: "4rem" }}>
+          Nice to meet you. Something about me.
+        </h1>
         <div class="row mt-4">
           <div class="col-lg-4">
             <img src={image} className={style["imageAboutPage"]} alt="" />
           </div>
 
           <div class="col-lg-8">
-            <p>
-              {" "}
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book. It has survived not
-              only five centuries, but also the leap into electronic
-              typesetting, remaining essentially unchanged
-            </p>
-            <div class="row mt-3">
-              <div class="col-md-6">
-                <ul>
-                  <li>Name: David Parker</li>
-                  <li>Age: 28</li>
-                  <li>Occupation: Web Developer</li>
-                </ul>
-              </div>
-              <div class="col-md-6">
-                <ul>
-                  <li>Name: David Parker</li>
-                  <li>Age: 28</li>
-                  <li>Occupation: Web Developer</li>
-                </ul>
-              </div>
-            </div>
             <div class="row mt-3">
               <p>
                 {" "}
-                Lorem Ipsum has been the industry's standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries, but also the leap into electronic
-                typesetting, remaining essentially unchanged. Lorem Ipsum has
-                been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it
-                to make a type specimen book. It has survived not only five
-                centuries, but also the leap into electronic typesetting,
-                remaining essentially unchanged.
+                I'm Christian Arruzza and I'm a Front End Developer since 2013.
+                I really love my job and I try to learn every day something new.
+                I started with plain HTML, Javascript and CSS, then I move
+                forward learning Vue js, Angular and React. I'm curios,
+                passionate, precise in my work and I love working in team.
               </p>
             </div>
+            {/* <div class="row mt-3">
+              <div class="col-md-6">
+                <ul>
+                  <li>Name: David Parker</li>
+                  <li>Age: 28</li>
+                  <li>Occupation: Web Developer</li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+                <ul>
+                  <li>Name: David Parker</li>
+                  <li>Age: 28</li>
+                  <li>Occupation: Web Developer</li>
+                </ul>
+              </div>
+            </div> */}
           </div>
         </div>
       </div>
