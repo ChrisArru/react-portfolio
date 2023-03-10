54cb62cd9e8a676066cbadf55fc3492d68462b5f
Christian Arruzza <christian.arruzza@gmail.com>
2023-03-10 11:49:25 +0100

    feat: add awesome fonts

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -1,25 +1,29 @@
 import React from "react";
 import "./Footer.css";
+import {
+  faGithubAlt,
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
   return (
     <footer id="footer">
       <div class="container-fluid">
         <div class="social-icons mt-4">
-          <a href="https://www.facebook.com/" target="_blank">
-            <i class="fab fa-facebook"></i>
+          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faFacebook} />
           </a>
-          <a href="https://www.instagram.com/" target="_blank">
-            <i class="fab fa-instagram"></i>
+          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faInstagram} />
           </a>
-          <a href="https://www.twitter.com/" target="_blank">
-            <i class="fab fa-twitter"></i>
+          <a href="https://www.github.com/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faGithubAlt} />
           </a>
-          <a href="https://www.linkedin.com/" target="_blank">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://www.twitch.tv/" target="_blank">
-            <i class="fab fa-twitch"></i>
+          <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} />
           </a>
         </div>
       </div>
