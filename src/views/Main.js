86ac93b93295ffa282e6a3c5d96a0f00ebc0c5d3
Christian Arruzza <christian.arruzza@gmail.com>
2023-04-03 21:06:20 +0200

    remove hr tag in mobile view

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import style from "./Main.module.css";
 import image from "../images/heroImage.jpg";
 
 const Main = () => {
+  const [screenSize, setScreenSize] = useState(getCurrentDimension());
+
+  const isRowVisible = useMemo(() => screenSize.width > 600, [screenSize]);
+
+  function getCurrentDimension() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  }
+
+  useEffect(() => {
+    const updateDimension = () => {
+      setScreenSize(getCurrentDimension());
+    };
+
+    window.addEventListener("resize", updateDimension);
+
+    return () => {
+      window.removeEventListener("resize", updateDimension);
+    };
+  }, [screenSize]);
+
   return (
     <section className={style["hero-jumbotron"]} id="home">
       <div class="container-fluid">
@@ -17,14 +40,16 @@ const Main = () => {
               </p>
             </div>
           </div> */}
-          <div class="jumbotron">
+          <div id="test" class="jumbotron">
             <h1 className={`${style["hero-title"]} display-4`}>
               Hello, world! I'm Christian.
             </h1>
             <p className={`${style["hero-desc"]} lead`}>
               Front End Developer based in Turin, Italy.
             </p>
-            <hr class="my-4" />
+            {isRowVisible && (
+              <hr class="my-4" style={{ display: "none !important" }} />
+            )}
             <p className={style["little-img"]}>
               <img src={image} style={{ width: "60%" }} alt="" />
             </p>
